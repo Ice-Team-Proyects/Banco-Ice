da72f07b331e1f3173392ca4032618676fef3720
kcuyuch-2024177 <kcuyuch-2024177@kinal.edu.gt>
2026-02-26 15:26:02 -0600

    docs: Se actualizo descripción del README.md

--- Sistema Bancario ICE/authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ Sistema Bancario ICE/authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4845e148e30720d0ec115de9d1af2e8c83172b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926cd7558dbb109dc268c8759e4f494778a6bc3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
