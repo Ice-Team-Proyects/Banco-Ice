23303ab4370f6e93eda89202c0934e18783f4510
cpatal-2024136 <cpatal-2024136@kinal.edu.gt>
2026-03-01 23:37:58 -0600

    Fix :Se arregla archivo README.md

--- Sistema Bancario ICE/authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ Sistema Bancario ICE/authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4845e148e30720d0ec115de9d1af2e8c83172b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5edd315349a2db4247102fd9ce87588850ccb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
